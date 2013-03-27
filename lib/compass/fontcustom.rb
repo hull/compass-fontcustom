64a307863a7b13df7c59a92f090086a68efefe7b
glaszig <glaszig@gmail.com>
2013-03-27 23:39:45 +0100

    refactored css templates, registering ourselves as a compass framework

--- lib/compass/fontcustom.rb
+++ lib/compass/fontcustom.rb
@@ -7,5 +7,7 @@ require "compass/fontcustom/font_importer"
 module Compass
   module Fontcustom
 		Sass.load_paths << FontImporter.new
+		base_directory  = File.expand_path('../../../', __FILE__)
+		Compass::Frameworks.register('fontcustom', :path => base_directory)
   end
 end
